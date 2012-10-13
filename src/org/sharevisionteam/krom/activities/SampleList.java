c58b0087a285171ce6bcb8bc271e9c1e2b4f680a
Dane Savot <dnnddane@gmail.com>
2012-10-13 15:59:36 +0700

    Create SimpleList activity for testing purpose.

---
+++ src/org/sharevisionteam/krom/activities/SampleList.java
@@ -0,0 +1,16 @@
+package org.sharevisionteam.krom.activities;
+
+import org.sharevisionteam.krom.R;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class SampleList extends ListActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.provinces)));
+	}
+	
+}
